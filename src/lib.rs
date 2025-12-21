183908feff9bbf1e94c1deff1c4da62e733d1719
Stephane Legrand <slegrand45@gmail.com>
2025-12-21 10:56:07 +0100

    Renaming

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,20 @@
 use ext_php_rs::prelude::*;
 
 #[php_class]
-pub struct PhpAmbientAuthority {
+pub struct CapStdAmbientAuthority {
     pub authority: cap_std::AmbientAuthority,
 }
 
 #[php_function]
-pub fn get_ambient_authority() -> PhpAmbientAuthority {
-    PhpAmbientAuthority {
+pub fn cap_std_ambient_authority() -> CapStdAmbientAuthority {
+    CapStdAmbientAuthority {
         authority: cap_std::ambient_authority(),
     }
 }
 
 #[php_module]
 pub fn get_module(module: ModuleBuilder) -> ModuleBuilder {
-    module.function(wrap_function!(get_ambient_authority))
+    module
+        .function(wrap_function!(cap_std_ambient_authority))
+        .class::<CapStdAmbientAuthority>()
 }
