f66641b64ae1090ec35ff504c74965acfb685f8e
Stephane Legrand <slegrand45@gmail.com>
2025-12-21 11:25:19 +0100

    Add open_ambient_dir()

--- php/test.php
+++ php/test.php
@@ -2,9 +2,14 @@
 
 // php -d extension=./target/debug/libphp_cap_std.so php/test.php
 
-$ambient_auth = cap_std_ambient_authority();
-var_export($ambient_auth);
+$ROOT = '/tmp';
 
+$ambient_auth = cap_std_ambient_authority();
 if (! is_a($ambient_auth, 'CapStdAmbientAuthority')) {
     die('cap_std_ambient_authority() should return an CapStdAmbientAuthority object');
+}
+
+$dir = cap_std_open_ambient_dir($ambient_auth, $ROOT);
+if (! is_a($dir, 'CapStdDir')) {
+    die('cap_std_open_ambient_dir() should return an CapStdDir object');
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,11 @@ pub struct CapStdAmbientAuthority {
     pub authority: cap_std::AmbientAuthority,
 }
 
+#[php_class]
+pub struct CapStdDir {
+    pub inner: cap_std::fs::Dir,
+}
+
 #[php_function]
 pub fn cap_std_ambient_authority() -> CapStdAmbientAuthority {
     CapStdAmbientAuthority {
@@ -14,9 +19,18 @@ pub fn cap_std_ambient_authority() -> CapStdAmbientAuthority {
     }
 }
 
+#[php_function]
+pub fn cap_std_open_ambient_dir(auth: &CapStdAmbientAuthority, path: String) -> Result<CapStdDir, String> {
+    let dir = cap_std::fs::Dir::open_ambient_dir(&path, auth.authority)
+        .map_err(|e| format!("Unable to open '{}' : {}", path, e))?;
+    Ok(CapStdDir { inner: dir })
+}
+
 #[php_module]
 pub fn get_module(module: ModuleBuilder) -> ModuleBuilder {
     module
         .function(wrap_function!(cap_std_ambient_authority))
+        .function(wrap_function!(cap_std_open_ambient_dir))
         .class::<CapStdAmbientAuthority>()
+        .class::<CapStdDir>()
 }
